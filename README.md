fa9ade0d9d5aed64a7eaacba725f972e6d919079
dream <dreamily854@outlook.com>
2023-02-11 16:32:18 +0800

    1. Update README.md 2. Use argparse replace sys.argv

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mandoc2html
 
+by [Dream](https://github.com/dreamily854)
+
+## Start Usage 
+1. install [rich](https://rich.readthedocs.io) library 
+    run command `pip3 install rich`
+2. Clone this repo
+3. run command `python3 mandoc2html.py /usr/share/man/man1/xxx.1.gz > example.html`
+
+
--- mandoc2html.py
+++ mandoc2html.py
@@ -1,47 +1,72 @@
+# Dream mandoc to html
+
+
 import rich.console
 import rich.text
 import tempfile
 import subprocess
-import sys
 
-# print(sys.argv)
+import argparse
 
-tf = tempfile.NamedTemporaryFile("wt")
+parser = argparse.ArgumentParser()
+
+parser.add_argument("filename")
+
+
+console_tempfile = tempfile.TemporaryFile()
+
+console = rich.console.Console(record=True,width=80,file=console_tempfile)
 # console = rich.console.Console(record=True,width=80)
-console = rich.console.Console(record=True,width=80,file=tf)
+
+zcatOutputTemp = tempfile.TemporaryFile()
 
 
-inputfile = sys.argv[1]
-a = subprocess.check_output(f"zcat {inputfile} | groff -mandoc -Tutf8",shell=True)
-b = a.decode("utf-8")
 
 
-c = b.split("\x08")
-d = list()
+def dataprocess(input:bytes)->str:
+    b = input.decode("utf-8")
 
-countAll = len(c)
+    c = b.split("\x08")
+    d = list()
 
-for i in range(countAll):
-    if i == 0 :
-        d.append(c[i][:-1])
-        continue
+    countAll = len(c)
+
+    for i in range(countAll):
+        if i == 0 :
+            d.append(c[i][:-1])
+            continue
+        
+        charA = c[i-1][-1]
+        charB = c[i][0]
+        charO = ""
+        if charA == charB:
+            charO =f"\033[1m{charB}\033[0m"
+        elif charA=="_":
+            charO =f"\033[4m{charB}\033[0m"
+        else:
+            charO = charB
+        
+        if i == countAll-1:
+            d.extend([charO,c[i][1:]])
+        else:
+            d.extend([charO,c[i][1:-1]])
+    return "".join(d)
+
     
-    charA = c[i-1][-1]
-    charB = c[i][0]
-    charO = ""
-    if charA == charB:
-        charO =f"\033[1m{charB}\033[0m"
-    elif charA=="_":
-        charO =f"\033[4m{charB}\033[0m"
-    else:
-        charO = charB
-    
-    if i == countAll-1:
-        d.extend([charO,c[i][1:]])
-    else:
-        d.extend([charO,c[i][1:-1]])
-    
 
-console.print(rich.text.Text.from_ansi("".join(d)))
 
-print(console.export_html())
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    # print(args.filename)
+    # inputfile = ""
+    zcatOutput = subprocess.check_output(["zcat", args.filename])
+    zcatOutputTemp.write(zcatOutput)
+    zcatOutputTemp.flush()
+    zcatOutputTemp.seek(0)
+    groffOutput = subprocess.check_output(["groff","-mandoc","-Tutf8"],stdin=zcatOutputTemp)
+
+    # a = subprocess.check_output(f"zcat {args.filename} | groff -mandoc -Tutf8",shell=True)
+    console.print(rich.text.Text.from_ansi(dataprocess(groffOutput)))
+    # console.print(rich.text.Text.from_ansi(dataprocess(a)))
+    print(console.export_html())
